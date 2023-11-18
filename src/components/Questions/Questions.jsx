09824a8a2b0beac755124a3338816b9edd71b91b
armanphero <017911sksaifurrahman@gmail.com>
2023-11-17 21:38:54 -0800

    card added

--- src/components/Questions/Questions.jsx
+++ src/components/Questions/Questions.jsx
@@ -5,7 +5,7 @@ const Questions = () => {
         <div>
             <div className='mb-6'>
                 <h1 className='font-semibold text-3xl mb-2'><span className='font-bold'>Question : </span>What is the difference between props and state ?</h1>
-                <p className='text-xl'><span className='font-bold'>Answer : </span>Props are used to pass data from a parent component to a child component, while state is used to manage data within a component</p>
+                <p className='text-xl'><span className='font-bold'>Answer : </span>Props are used to pass data from a parent component to a child component, while state is used to manage data within a component.</p>
             </div>
             <div className='mb-6'>
                 <h1 className='font-semibold text-3xl mb-2'><span className='font-bold'>Question : </span>How Does useState() Work?</h1>
